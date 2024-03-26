6e414ed1bab030869ed4118485f54940afbe71d5
mursidin14 <54929754+mursidin14@users.noreply.github.com>
2024-03-26 15:24:33 +0800

    make logic login

--- app/Services/Impl/UserServiceImpl.php
+++ app/Services/Impl/UserServiceImpl.php
@@ -6,5 +6,17 @@ use App\Services\UserService;
 
 class UserServiceImpl implements UserService
 {
-    
+    private array $user = [
+        'mursidin' => '123456'
+    ];
+
+    function login(string $usernmae, string $password): bool
+    {
+        if(!isset($this->user[$usernmae])) {
+            return false;
+        }
+
+        $correctPassword = $this->user[$usernmae];
+        return $password == $correctPassword;
+    }
 }
--- tests/Feature/UserServiceTest.php
+++ tests/Feature/UserServiceTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use App\Services\UserService;
+use Illuminate\Auth\Events\Login;
 use Tests\TestCase;
 
 class UserServiceTest extends TestCase
@@ -18,8 +19,18 @@ class UserServiceTest extends TestCase
         $this->userService = $this->app->make(UserService::class);
     }
 
-    public function testSample()
+    public function testLoginSuccess()
     {
-        self::assertTrue(true);
+        self::assertTrue($this->userService->login('mursidin', '123456'));
+    }
+
+    public function testLoginNotFoung()
+    {
+        self::assertFalse($this->userService->login('salah', 'salah'));
+    }
+
+    public function testLoginWrongPassword()
+    {
+        self::assertFalse($this->userService->login('mursidin', 'salah'));
     }
 }
