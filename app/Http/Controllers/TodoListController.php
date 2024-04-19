6068999e06a30487d5277f38a282c8d395ef5df8
mursidin14 <54929754+mursidin14@users.noreply.github.com>
2024-04-19 22:51:47 +0800

    add action data todolist

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -25,7 +25,20 @@ class TodoListController extends Controller
 
     public function addTodoList(Request $request)
     {
+        $todo = $request->input('todo');
 
+        if(empty($todo)){
+            $todoList = $this->todoListService->getTodoList();
+            return response()->view('todolis.todolist', [
+                'title' => 'TodoList',
+                'todolist' => $todoList,
+                'error' => 'Todo is required'
+            ]);
+        }
+
+        $this->todoListService->saveTodo(uniqid(), $todo);
+
+        return redirect()->action([TodoListController::class, 'todoList']);
     }
 
     public function removeTodoList(Request $request, string $todoId)
--- tests/Feature/TodolistControllerTest.php
+++ tests/Feature/TodolistControllerTest.php
@@ -19,4 +19,21 @@ class TodolistControllerTest extends TestCase
         ])->get('/todolist')
         ->assertSeeText('1')->assertSeeText('asrul');
     }
+
+    public function testTodoListFiled()
+    {
+        $this->withSession([
+            'username' => 'mursidin'
+        ])->post('/todolist', [])
+          ->assertSeeText('Todo is required');
+    }
+
+    public function testAddTodoSuccess()
+    {
+        $this->withSession([
+            'username' => 'mursidin'
+        ])->post('/todolist', [
+            'todo' => 'asrul'
+        ])->assertRedirect('/todolist');
+    }
 }
